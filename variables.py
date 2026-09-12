55fc4378a52e7f93e082b7a646cc7aa5c15b9cd4
beafreitass <bfreitas@mail.valenciacollege.edu>
2026-09-11 20:42:35 -0400

    Variables - Work 2

---
+++ variables.py
@@ -0,0 +1,47 @@
+first_number = int(input("Please enter the first number\n"))
+second_number = int(input("Please enter the second number\n"))
+
+answer = first_number * second_number
+print("The answer to your query is ", answer)
+
+print()
+
+first_number = int(input("Please enter the first number\n"))
+second_number = int(input("Please enter the second number\n"))
+
+answer = first_number / second_number
+print("The answer to your query is ", answer)
+
+print()
+
+first_number = int(input("Please enter the first number\n"))
+second_number = int(input("Please enter the second number\n"))
+
+answer = first_number // second_number
+print("The answer to your query is ", answer)
+
+print()
+
+first_number = int(input("Please enter the first number\n"))
+second_number = int(input("Please enter the second number\n"))
+
+answer = first_number % second_number
+print("The answer to your query is ", answer)
+
+print()
+
+first_number = int(input("Please enter the first number\n"))
+second_number = int(input("Please enter the second number\n"))
+
+answer = first_number + second_number
+print("The answer to your query is ", answer)
+
+print()
+
+first_number = int(input("Please enter the first number\n"))
+second_number = int(input("Please enter the second number\n"))
+
+answer = first_number - second_number
+print("The answer to your query is ", answer)
+
+print("Beatriz")
